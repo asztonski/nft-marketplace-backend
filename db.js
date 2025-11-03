15d7c90d12a75868589967864e334260ffc42f83
asztonski <asztonski@gmail.com>
2025-11-03 16:11:08 -0500

    Refactor database name assignment in MongoDB connection to use a constant variable

--- db.js
+++ db.js
@@ -4,13 +4,14 @@ const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?appName=${process.env.MONGO_APP_NAME}`;
 
 const client = new MongoClient(uri);
+const DB_NAME = "nft";
 
 let db;
 
 async function connectDB() {
   try {
     await client.connect();
-    db = client.db("nft");
+    db = client.db(DB_NAME);
     console.log("✅ Connected to MongoDB Atlas");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
