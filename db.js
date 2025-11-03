25ad79a9e78b704c8d4840ca5d1fb6feca909f88
asztonski <asztonski@gmail.com>
2025-11-03 15:30:16 -0500

    Add database connection module for MongoDB

---
+++ db.js
@@ -0,0 +1,26 @@
+// db.js
+const { MongoClient } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?appName=Cluster0`;
+// np. mongodb+srv://username:password@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority
+
+const client = new MongoClient(uri);
+
+let db;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db(); // jeśli w URI nie podałeś nazwy DB, możesz dodać: client.db("nazwaDB")
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+}
+
+function getDB() {
+  if (!db) throw new Error("Database not connected. Call connectDB() first.");
+  return db;
+}
+
+module.exports = { connectDB, getDB };
