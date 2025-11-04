b295760476fc8b5e00b30028bca1d4b4435b00a2
asztonski <asztonski@gmail.com>
2025-11-04 12:43:34 -0500

    Fix import case sensitivity for User model in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const User = require("./models/User");
+const User = require("./models/user");
 
 const port = process.env.PORT || 3000;
 
