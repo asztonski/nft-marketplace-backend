f00927fd9f9d223aa7e71d9bc3fd9a64330e7c67
asztonski <asztonski@gmail.com>
2025-12-05 21:07:45 +0100

    Add EmailService to central export for user services

--- services/modules/index.js
+++ services/modules/index.js
@@ -8,10 +8,12 @@ const UserRepository = require("./userRepository");
 const UserValidator = require("./userValidator");
 const UsernameGenerator = require("./usernameGenerator");
 const UserMigration = require("./userMigration");
+const EmailService = require("../email/emailService");
 
 module.exports = {
   UserRepository,
   UserValidator,
   UsernameGenerator,
   UserMigration,
+  EmailService,
 };
